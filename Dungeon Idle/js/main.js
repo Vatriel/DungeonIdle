dc16458df01b9fc63ec52724759ab188bfba4c35
Vatriel <victorallard.410@gmail.com>
2025-07-15 18:54:45 +0200

    Delete Dungeon Idle directory

--- Dungeon Idle/js/main.js
+++
@@ -1,7 +0,0 @@
-import { initGame } from './core/Game.js';
-
-// Attend que le HTML soit entièrement chargé avant de lancer le jeu
-document.addEventListener('DOMContentLoaded', () => {
-  console.log("Le DOM est chargé, lancement du jeu.");
-  initGame();
-});
